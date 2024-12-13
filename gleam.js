9b8749cc21bfd5670c8a3604d86f5ec321f37319
McKayla Washburn <mckayla@hey.com>
2024-12-12 20:50:21 -0700

    tons of improvements
    - better string highlighting
    - multiline strings
    - new keywords
    - @ "decorators"
    - several other small fixes

--- gleam.js
+++ gleam.js
@@ -1,37 +1,59 @@
+// Up to date as of Gleam v1.6.3
 Prism.languages.gleam = {
-	doc: {
-		pattern: /\/\/\/\/?.*/,
-		greedy: true,
-	},
 	comment: {
-		pattern: /\/\/.*/,
+		pattern: /\/\/\/?.*/,
 		greedy: true,
 	},
-	function: /([a-z_][a-z0-9_]+)(?=\()/,
+
+	tag: /@(deprecated|external|internal)/,
+
+	target: {
+		pattern: /\b(erlang|javascript)\b/,
+		alias: "symbol"
+	},
+
+	function: /\b(?<!@)([a-z_][a-z0-9_]+)(?=\()/,
+
+	// Keyword list taken from https://github.com/gleam-lang/gleam/blob/v1.6.3/compiler-core/src/parse/lexer.rs#L23
 	keyword:
-		/\b(use|case|if|external|fn|import|let|assert|try|pub|type|opaque|const|todo|as)\b/,
-	symbol: {
-		pattern: /([A-Z][A-Za-z0-9_]+)/,
+		/\b(as|assert|auto|case|const|delegate|derive|echo|else|fn|if|implement|import|let|macro|opaque|panic|pub|test|todo|type|use)\b/,
+
+	type: {
+		pattern: /(?:[A-Z][A-Za-z0-9_]+)/,
 		greedy: true,
+		alias: "symbol",
 	},
-	operator: {
-		pattern:
-			/(<<|>>|<-|->|\|>|<>|\.\.|<=\.?|>=\.?|==\.?|!=\.?|<\.?|>\.?|&&|\|\||\+\.?|-\.?|\/\.?|\*\.?|%\.?|=)/,
-		greedy: true,
+
+	namespace: {
+		pattern: /(?:[a-z][a-z0-9_]*)\.(?!{)/,
+		inside: {
+			punctuation: /\./,
+		},
 	},
+
+	punctuation: /[:!,{}()\[\]]/,
+
 	string: {
 		pattern: /"(?:\\(?:\r\n|[\s\S])|(?!")[^\\\r\n])*"/,
 		greedy: true,
 	},
-	module: {
-		pattern: /([a-z][a-z0-9_]*)\.(?!{)/,
-		inside: {
-			punctuation: /\./,
-		},
-		alias: "keyword",
+
+	operator: {
+		pattern:
+			/(<<|>>|<-|->|\|>|<>|\.\.|<=\.?|>=\.?|==\.?|!=\.?|<\.?|>\.?|&&|\|\||\+\.?|-\.?|\/\.?|\*\.?|%|=|\|)/,
+		greedy: true,
 	},
-	punctuation: /[.\\:,{}()]/,
+
 	number:
 		/\b(?:0[bB][01_]+|0[oO][0-7_]+|\d[\d_]*(\\.[\d_]*(e-?[\d_]+)?)?|0[xX][\da-fA-F_]+)\b/,
+
 	boolean: /\b(?:True|False|Ok|Error|Nil)\b/,
+
+	string: {
+		pattern: /"[\s\S]*?(?<!\\)"/,
+		inside: {
+			punctuation: /\\./,
+		},
+		greedy: true,
+	},
 };
